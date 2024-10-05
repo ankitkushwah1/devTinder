c29292bfada26d5c25362e308606a559339e9c99
Ankit Kushwah <ankitkushwah9856@gmail.com>
2024-10-06 00:04:35 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,11 +2,34 @@ const express = require("express");
 
 const app = express();
 
-// This will only match GET call to /user
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstname: "ankit", lastname: "kushwah" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    //Route Handler
+    console.log("Handling the route user!!");
+    // res.send();
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!");
+    // res.send("2nd response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 3!");
+    // res.send("3nd response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4!");
+    // res.send("4nd response!!");
+    next();
+  },
+  (req, res) => {
+    console.log("Handling the route user 5!");
+    res.send("5nd response!!");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is sucessfully listening on port 3000 ...");
