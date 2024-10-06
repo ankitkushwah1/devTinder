dc3612c2474997165520e050eadca7964eee16e7
Ankit Kushwah <ankitkushwah9856@gmail.com>
2024-10-06 11:42:32 +0530

    Wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -3,22 +3,20 @@ const { adminAuth, userAuth } = require("./middleware/auth");
 
 const app = express();
 
-// Handle Auth Middleware for all request GET, POST, PATCH , DELETE
-app.use("/admin", adminAuth);
-app.post("/user/login", (req, res) => {
-  res.send("User logged in successfully ");
-});
-app.get("/user", userAuth, (req, res) => {
-  res.send("User Data Sent");
-});
-app.get("/admin/getAllData", (req, res) => {
-  // Login of checking if the request is authorized
-  res.send("all data sent");
+app.get("/getUserData", (req, res) => {
+  try {
+    throw new Error("Random Error");
+    res.send("User Data sent");
+  } catch (err) {}
+  // Logic of DB call and get user data
+  res.status(500).send("Some Error contact support team");
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-  // Login of checking if the request is authorized
-  res.send("Deleted Data User");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your Error
+    res.status(500).send("something went wrong");
+  }
 });
 
 app.listen(3000, () => {
